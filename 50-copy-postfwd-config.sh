ec6c87215ee3a85fe77582310ee1cab6ba45b30a
Juraj Ontkanin <Juraj_Ontkanin@bcit.ca>
2019-06-26 16:01:29 -0700

    use CONFIG dir instead of hardcoded path

--- 50-copy-postfwd-config.sh
+++ 50-copy-postfwd-config.sh
@@ -1,3 +1,3 @@
-TEMP_CONFFILE=/config/postfwd.cf
+TEMP_CONFFILE="${CONFIGDIR}/postfwd.cf"
 [ -f "$TEMP_CONFFILE" ] && { cp $TEMP_CONFFILE ${ETC}/${CONF};chown -R root:${GID} ${ETC}/${CONF};chmod 0640 ${ETC}/${CONF};echo "copied custom: $TEMP_CONFFILE"; } \
 	|| echo "using default: ${ETC}/${CONF}"
